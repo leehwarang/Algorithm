86ea6e6b7ef99ae639dacf18b6231400faecd3fd
michelle <hwrng2@gmail.com>
2018-06-26 13:31:43 +0900

    Programmers level1 - 문자열 다루기 기본

---
+++ level1/1-10(unsolved).py
@@ -0,0 +1,19 @@
+'''
+Prorgammers level1
+문제: 문자열 다루기 기본(문자와 숫자 구별)
+'''
+
+def solution(s):
+      
+    if len(s) == 4:
+        for i in s:
+            try:
+                float(s)
+            except ValueError:
+                return False
+        return True
+    else: 
+        return False
+
+solution("a234")
+solution("1234")
